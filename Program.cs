0dca973fa71b1a70bbc099ade088041ac43b4e91
Admin <Admin@WIN-0QFN66U68EP>
2024-11-23 01:23:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер члена ряда Фибоначчи (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int fibonacciNumber = Fibonacci(n);
+        Console.WriteLine($"n-ый член ряда Фибоначчи (F({n})) = {fibonacciNumber}");
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
